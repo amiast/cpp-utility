70bda99c2618633e4c64e1e83d1e46fa58464949
ttineres <martsaicus@gmail.com>
2025-10-10 19:49:40 +0800

    Update internal_type_traits.hpp

--- kotone/internal_type_traits.hpp
+++ kotone/internal_type_traits.hpp
@@ -9,6 +9,29 @@ template <typename T> concept number = std::is_arithmetic_v<T>;
 
 template <typename T> concept signed_number = std::signed_integral<T> || std::floating_point<T>;
 
+template <typename mint> concept compatible_modint = requires(mint a, mint b, int64_t x) {
+    { mint::mod() } -> std::convertible_to<int>;
+    mint{1};
+    mint{x};
+
+    { a + b }       -> std::same_as<mint>;
+    { a - b }       -> std::same_as<mint>;
+    { -a }          -> std::same_as<mint>;
+    { a * b }       -> std::same_as<mint>;
+    { a / b }       -> std::same_as<mint>;
+    { a.inv() }     -> std::same_as<mint>;
+    { a.pow(x) }    -> std::same_as<mint>;
+
+    { a += b }      -> std::same_as<mint&>;
+    { a -= b }      -> std::same_as<mint&>;
+    { a *= b }      -> std::same_as<mint&>;
+    { a /= b }      -> std::same_as<mint&>;
+
+    { a == b }      -> std::convertible_to<bool>;
+    { a != b }      -> std::convertible_to<bool>;
+    { a.val() }     -> std::convertible_to<int>;
+};
+
 }  // namespace kotone
 
 #endif  // KOTONE_INTERNAL_TYPE_TRAITS
