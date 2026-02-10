86eb7e799b2d9105d8a9edfbeb8d391ae37e803c
amiast <martsaicus@gmail.com>
2026-02-11 03:26:40 +0800

    Update internal_type_traits.hpp

--- kotone/internal_type_traits.hpp
+++ kotone/internal_type_traits.hpp
@@ -33,6 +33,27 @@ template <typename mint> concept compatible_modint = requires(mint a, mint b, in
     { a.val() }     -> std::convertible_to<int>;
 };
 
+template <typename T> concept additive = requires(T a, T b) {
+    T{};
+    { a + b }       -> std::same_as<T>;
+    { a - b }       -> std::same_as<T>;
+    { -a }          -> std::same_as<T>;
+    { a == b }      -> std::convertible_to<bool>;
+    { a != b }      -> std::convertible_to<bool>;
+};
+
+template <typename T> concept mutable_additive = (
+    additive<T>
+    && requires(T a, T b) {
+        { a += b }  -> std::same_as<T&>;
+        { a -= b }  -> std::same_as<T&>;
+        { ++a }     -> std::same_as<T&>;
+        { --a }     -> std::same_as<T&>;
+        { a++ }     -> std::same_as<T>;
+        { a-- }     -> std::same_as<T>;
+    }
+);
+
 }  // namespace kotone
 
 #endif  // KOTONE_INTERNAL_TYPE_TRAITS
