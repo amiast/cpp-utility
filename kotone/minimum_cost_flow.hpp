7b461677fae1bfd1e7ea1c007eadf7bd7b45c16b
amiast <martsaicus@gmail.com>
2026-09-10 01:28:43 +0800

    Update minimum_cost_flow.hpp

--- kotone/minimum_cost_flow.hpp
+++ kotone/minimum_cost_flow.hpp
@@ -6,6 +6,7 @@
 #include <tuple>
 #include <cassert>
 #include <atcoder/mincostflow>
+#include "ortools/graph/min_cost_flow.h"
 
 namespace kotone {
 
@@ -34,6 +35,27 @@ template <typename Cap, typename Cost> struct mincost_network_graph {
     std::vector<Cap> _balance, _flow;
     std::vector<bool> _rev;
     Cost _cost{};
+
+    std::vector<Cost> _to_potential(std::vector<std::vector<std::pair<int, Cost>>> &&residual) const {
+        std::vector<Cost> potential(num_nodes());
+        std::queue<int> queue;
+        for (int i = 0; i < num_nodes(); i++) queue.push(i);
+        std::vector<bool> in_queue(num_nodes(), true);
+        while (queue.size()) {
+            int u = queue.front();
+            queue.pop();
+            in_queue[u] = false;
+            for (auto &[v, c] : residual[u]) {
+                if (potential[u] + c >= potential[v]) continue;
+                potential[v] = potential[u] + c;
+                if (!in_queue[v]) {
+                    queue.push(v);
+                    in_queue[v] = true;
+                }
+            }
+        }
+        return potential;
+    }
 
   public:
     mincost_network_graph(int num_nodes) {
@@ -119,23 +141,32 @@ template <typename Cap, typename Cost> struct mincost_network_graph {
             result.flow[i] += edge.flow;
             if (_rev[i]) result.flow[i] = -result.flow[i];
         }
-        result.potential.resize(num_nodes());
-        std::queue<int> queue;
-        for (int i = 0; i < num_nodes(); i++) queue.push(i);
-        std::vector<bool> in_queue(num_nodes(), true);
-        while (queue.size()) {
-            int u = queue.front();
-            queue.pop();
-            in_queue[u] = false;
-            for (auto &[v, c] : residual[u]) {
-                if (result.potential[u] + c >= result.potential[v]) continue;
-                result.potential[v] = result.potential[u] + c;
-                if (!in_queue[v]) {
-                    queue.push(v);
-                    in_queue[v] = true;
-                }
-            }
+        result.potential = _to_potential(std::move(residual));
+        return result;
+    }
+
+    // Computes and returns a minimum-cost flow in the network via cost-scaling push-relabel.
+    // Requires Google OR-Tools `"ortools/graph/min_cost_flow.h"`.
+    // Requires `Cap` and `Cost` to be integer types representable by `int64_t`.
+    // Reference: https://or-tools.github.io/docs/cpp/min__cost__flow_8h_source.html
+    mincost_flow flow_cspr() {
+        using cspr_graph = operations_research::SimpleMinCostFlow;
+        cspr_graph graph(num_nodes());
+        for (auto &[u, v, cap, cost] : _edges) graph.AddArcWithCapacityAndUnitCost(u, v, cap, cost);
+        for (int v = 0; v < num_nodes(); v++) graph.SetNodeSupply(v, _balance[v]);
+        if (int status = graph.Solve(); status != cspr_graph::OPTIMAL) return {};
+        mincost_flow result{_flow, {}, _cost + graph.OptimalCost(), true};
+        std::vector<std::vector<std::pair<int, Cost>>> residual(num_nodes());
+        for (int i = 0; i < num_edges(); i++) {
+            int tail = graph.Tail(i), head = graph.Head(i);
+            Cap flow = graph.Flow(i), cap = graph.Capacity(i);
+            Cost cost = graph.UnitCost(i);
+            if (flow < cap) residual[tail].emplace_back(head, cost);
+            if (flow > Cap{}) residual[head].emplace_back(tail, -cost);
+            result.flow[i] += flow;
+            if (_rev[i]) result.flow[i] = -result.flow[i];
         }
+        result.potential = _to_potential(std::move(residual));
         return result;
     }
 };
